8bfb3053891d6627b1cb680c3e8a4f7c5c2ee683
Seyfullah Ismailov <seyfullahismailly@gmail.com>
2026-03-11 15:42:01 +0300

    added files in branch hw_14_with_selenoid

--- tests/resources/conftest.py
+++ tests/resources/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
 from utils import attaches
+import os
 
 
 @pytest.fixture(autouse=True)
@@ -11,7 +11,12 @@ def browser_management():
     options = Options()
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-notifications")
-    options.add_argument("--start-maximized")
+
+    # Добавим поддержку headless для CI
+    if os.getenv('CI'):
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -28,17 +33,15 @@ def browser_management():
         options=options
     )
 
-    # browser.config.driver_options = options
-    # browser.config.base_url = 'https://mwi.me/'
     browser.config.driver = driver
     browser.config.timeout = 10
+    browser.config.base_url = 'https://mwi.me/'  # Добавим base_url
 
     yield
 
-    if browser.driver:
-        attaches.add_screenshot(browser.driver)
-        attaches.add_page_source(browser.driver)
-        attaches.add_logs(browser.driver)
-        attaches.add_video(browser.driver)
+    attaches.add_screenshot(driver)
+    attaches.add_page_source(driver)
+    attaches.add_logs(driver)
+    attaches.add_video(driver)
 
     browser.quit()
